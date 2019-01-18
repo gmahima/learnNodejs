0190b47cdf63ab345c55fb724f1cbe6928434223
Mahima <mahimagangavarapu.com>
2019-01-18 16:00:23 +0000

    bind

--- renderer.js
+++ renderer.js
@@ -1,8 +1,15 @@
 var fs = require('fs');
+function mergeValues (values, content) {
+  for (var key in values) {
+    console.log(values[key]);
+    content.replace("{{" + key + "}}", values[key]);
+   
+  }
+ return content;
+}
 function view (templateName, values, response) {
-  
-var fileContents = fs.readFileSync('./views/' + templateName + '.html');
-
+var fileContents = fs.readFileSync('./views/' + templateName + '.html', {encoding: "utf8"});
+fileContents = mergeValues(values, fileContents);
   response.write(fileContents);
 
 }
